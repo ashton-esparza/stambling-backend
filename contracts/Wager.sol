9e64720e0248b00a410af4cdcf6bdde659dacec0
Ashton Esparza <ashton.esparza@yahoo.com>
2022-09-04 15:04:59 -0400

    modified contract to push player array correctly and added solhint

--- contracts/Wager.sol
+++ contracts/Wager.sol
@@ -15,11 +15,18 @@ contract Wager {
   /* Type Declarations */
   struct Player {
     address s_playerAddress;
-    uint256 s_playerWager;
+    uint256 s_playerPrediction;
+  }
+  enum WagerState {
+    REGISTERING,
+    ACTIVE,
+    CALCULATING,
+    COMPLETE
   }
 
   /* State Variables */
   AggregatorV3Interface private immutable i_priceFeed;
+  uint32 private constant MAX_NUM_PLAYERS = 2;
 
   /* Wager Variables */
   uint256 private s_predictionA;
@@ -34,5 +41,12 @@ contract Wager {
     s_lastTimeStamp = block.timestamp;
   }
 
-  function enterWager(address playerAddress, uint256 playerPrediction) public {}
+  function enterWager(address playerAddress, uint256 playerPrediction) public {
+    if (s_players.length >= MAX_NUM_PLAYERS) {
+      revert Wager__Full();
+    }
+    //add functionality to ensure correct amount of ether is sent
+    s_players.push(Player({s_playerAddress: playerAddress, s_playerPrediction: playerPrediction}));
+    //s_players = new address payable[](0); to reset array later
+  }
 }
