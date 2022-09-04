9c1473dd2c7f5c648c12171ce1f8d7e96293e476
Ashton Esparza <ashton.esparza@yahoo.com>
2022-09-04 15:41:22 -0400

    successfully deployed contract on hardhat

--- deploy/01-deploy-wager.js
+++ deploy/01-deploy-wager.js
@@ -1,11 +1,34 @@
 const { network, ethers } = require("hardhat");
-const { developmentChains, networkConfig } = require("../helper-hardhat.config");
+const { developmentChains, networkConfig } = require("../helper-hardhat-config");
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
+  let ethUsdPriceFeedAddress;
 
+  log("----------------START-------------------------------");
   if (developmentChains.includes(network.name)) {
+    // Get locally deployed mock data feed contract
+    log("Getting address for locally deployed ethUsdAggregator contract...");
+    // const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+    // ethUsdPriceFeedAddress = ethUsdAggregator.address;
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+    log(`ethUsdPriceFeed is: ${ethUsdPriceFeedAddress}`);
+  } else {
+    log("done");
   }
+  log("----------------------------------------------------");
+  log("Deploying FundMe and waiting for confirmations...");
+  const arguments = [ethUsdPriceFeedAddress];
+  const wager = await deploy("Wager", {
+    from: deployer,
+    args: [ethUsdPriceFeedAddress],
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  });
+  log(`Wager deployed at ${wager.address} on network id ${chainId}`);
+  log("----------------END---------------------------------");
 };
+
+module.exports.tags = ["all", "wager"];
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -3,14 +3,17 @@ const { ethers } = require("hardhat");
 const networkConfig = {
   4: {
     name: "rinkeby",
-    vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+    ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+    btcUsdPriceFeed: "0xECe365B379E1dD183B20fc5f022230C044d51404",
   },
   5: {
     name: "goerli",
-    vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+    ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
+    btcUsdPriceFeed: "0xA39434A63A52E749F02807ae27335515BA4b07F7",
   },
   31337: {
     name: "hardhat",
+    ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
   },
 };
 
