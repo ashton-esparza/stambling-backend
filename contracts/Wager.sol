a15e0f5e878621edbcef175cd57e2240d8a50c4e
Ashton Esparza <ashton.esparza@yahoo.com>
2022-09-04 11:45:27 -0400

    Initial commit

---
+++ contracts/Wager.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+/**@title Stambling's Wager Smart Contract
+ * @author Ashton Esparza
+ * @notice This contract is for creating wagers within Stambling
+ * @dev This implements the Chainlink Data Feeds
+ */
+contract Wager {
+
+}
